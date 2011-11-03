e92aafc67fda6bdbabc7561f25039b1c1837e9e1
Fred Ross <fross@mephistopheles.(none)>
2011-11-03 09:50:07 -0700

    Added code that's on meph.

---
+++ seqviewer/ab1.py
@@ -0,0 +1,453 @@
+"""
+ab1.py - Read Applied BioSystems AB1 files
+Copyright 2011, Frederick Ross
+
+"""
+import collections
+import contextlib
+import threading
+import cStringIO
+import datetime
+import struct
+import pytest
+import math
+import sys
+
+major_version = 1
+
+def packed_handle(fmt, *args):
+    """Wrap a call to struct.pack in a temporary handle.
+
+    The struct module works with strings, while all the functions in
+    this module work on handles, so this function resolves the
+    mismatch.
+    """
+    return cStringIO.StringIO(struct.pack(fmt, *args))
+
+def test_packed_handle():
+    assert packed_handle('>i', 0).read() == '\0\0\0\0'
+    assert packed_handle('>B', ord('a')).read() == 'a'
+
+
+def parse_header(h):
+    """Parse the header of an AB1 file.
+
+    Returns the version number, the offset to the directory of entries
+    in the AB1 file, and the number of entries in that directory as a
+    dictionary (with keys 'version', 'numelements', and 'dataoffset').
+    """
+    # AB1 files begin with four bytes encoding the ASCII characters 'ABIF'
+    abif = h.read(4)
+    if abif != "ABIF":
+        raise ValueError("AB1 file does not begin with 'ABIF'")
+    # Next come two bytes encoding the file format version (multiplied
+    # by 100 so that 1.01 is stored as the integer 101).
+    version, = struct.unpack('>h', h.read(2))
+    if math.floor(version/100) != 1:
+        raise ValueError("AB1 file version is not supported by this "
+                         "library (found: %f, must be 1.xx)" % (version/100))
+    # Then comes a directory entry pointing to the offset containing
+    # all the entries for this file.
+    tdir = parse_direntry(h)
+    return {'version': version,
+            'numelements': tdir['numelements'],
+            'dataoffset': tdir['dataoffset']}
+
+def test_header():
+    valid_header = packed_handle('>cccc h ccccihhiiii',
+                                 'A','B','I','F', 101,
+                                 't', 'd', 'i', 'r',
+                                 1, 1023, 28, 10, 280, 10233, 30)
+    assert parse_header(valid_header) == {'version': 101,
+                                          'numelements': 10,
+                                          'dataoffset': 10233}
+    with pytest.raises(ValueError):
+        bad_abif = packed_handle('>cccch', 'A', 'B', 'Q', 'R', 101)
+        parse_header(bad_abif)
+        bad_version = packed_handle('>cccch', 'A','B','I','F', 205)
+
+
+# Next we define functions to decode all the supported data types in
+# the file.  All such decoder functions take two arguments, a handle
+# to read from and an integer giving the number of bytes to read, and
+# read a tuple of two values, the first being the value read and the
+# second the number of bytes consumed in doing so.
+
+# For types which decode to atomic objects like a single int, we use a
+# factory function to produce the decoders.
+def reader(fmt):
+    """Return a function to decode according to *fmt*.
+
+    The *fmt* argument is a string formatted according the usage of
+    the ``struct`` module.  For example, to create a function to read
+    a 32 bit, big Endian integer, you would call reader('>i').  The
+    resulting function takes two arguments: a handle to read and the
+    number of bytes which should be consumed.  It returns a tuple of
+    the value read and the number of bytes actually consumed.  For
+    instance,::
+
+        reader('>i')(packed_handle('>i', 1066))
+
+    evaluates to ``(1066, 4)``.
+    """
+    n = struct.calcsize(fmt)
+    def reader(h, size):
+        val = struct.unpack(fmt, h.read(n))[0]
+        return (val, n)
+    return reader
+
+def test_reader():
+    assert reader('>i')(packed_handle('>i', 1066), 4) == (1066, 4)
+
+
+def read_date(h, size):
+    """Read a date into a ``datetime.date`` object."""
+    assert size == 4
+    (year,month,day) = struct.unpack('>hBB', h.read(4))
+    return (datetime.date(year,month,day), 4)
+
+def test_read_date():
+    d = datetime.date(1066,6,20)
+    h = packed_handle('>hBB', d.year, d.month, d.day)
+    assert read_date(h, 4) == (d, 4)
+
+
+def read_time(h, size):
+    """Read a time into a ``datetime.time`` object."""
+    assert size == 4
+    (hour,minute,second,centisecond) = struct.unpack('>BBBB', h.read(4))
+    # time's last field is microseconds, not centiseconds
+    return (datetime.time(hour,minute,second,centisecond*10), 4)
+
+def test_read_time():
+    t = datetime.time(13,22,56,120)
+    h = packed_handle('>BBBB', t.hour, t.minute, t.second, t.microsecond/10)
+    assert read_time(h, 4) == (t, 4)
+
+
+def read_pstring(h, size):
+    """Read a Pascal style string from handle *h*.
+
+    The first byte read is taken to be the length of the string.  The
+    *size* argument should be 1 for valid files, but is ignored for
+    this function.  The number of bytes returned is the number of
+    characters plus one for the byte giving the length.
+    """
+    assert size == 1
+    first_byte = h.read(1)
+    if first_byte == '':
+        raise ValueError('Failed to read length for pstring')
+    n_chars = struct.unpack('>B', first_byte)[0]
+    return (h.read(n_chars), n_chars+1)
+
+def test_read_pstring():
+    s = 'abcdef'
+    assert len(s) < 256
+    h = cStringIO.StringIO(struct.pack('>B', len(s)) + s)
+    assert read_pstring(h, 1) == (s, 7)
+
+def read_cstring(h, size):
+    """Read a C style string from handle *h*.
+
+    The string continues until a terminating \x00 character is read.
+    The length returned includes the terminating character.
+    """
+    accumulated_string = ''
+    while True:
+        next_char = h.read(1)
+        if next_char == '\0' or next_char == '': # End of the string
+            break
+        accumulated_string += next_char
+    return (accumulated_string, len(accumulated_string)+1)
+
+def test_read_cstring():
+    s = 'abcdef'
+    h = cStringIO.StringIO(s + '\0')
+    assert read_cstring(h, 1) == (s, 7)
+
+
+# Legacy supported data types
+def read_thumb(h, size):
+    assert size == 10
+    return (struct.unpack('>iiBB', h.read(10)), 10)
+
+def read_bool(h, size):
+    assert size == 1
+    return (h.read(1) != 0, 1)
+
+def read_user(h, size):
+    """Returns raw strings of bytes."""
+    return (h.read(size), size)
+
+# We use a lookup table on the elementtype field to decide how to
+# decode a given element.
+_element_decoders = {1: reader('>B'), # unsigned byte
+                     2: reader('>b'), # signed byte
+                     3: reader('>H'), # unsigned short
+                     4: reader('>h'), # signed short
+                     5: reader('>i'), # signed 4 byte int
+                     7: reader('>f'), # 4 byte float
+                     8: reader('>d'), # 8 byte double precision float
+                     10: read_date,  # Date (year,month,day)
+                     11: read_time,   # Time (hour,minute,second,centisecond)
+                     18: read_pstring, # Pascal-style strings
+                     19: read_cstring, # C-style strings
+                     12: read_thumb, # Thumbprints (obsolete)
+                     13: read_bool, # Booleans (obsolete)
+                     1024: read_user # User defined data type
+                     }
+
+
+def parse_direntry(h):
+    """Parse a DirEntry from the AB1 file."""
+    # The format is a C struct of:
+    # struct DirEntry {
+    #     char[4] name;
+    #     signed int32 number;
+    #     signed int16 elementtype;
+    #     signed int16 elementsize;
+    #     signed int32 numelements;
+    #     signed int32 datasize;
+    #     signed int32 dataoffset;
+    #     signed int32 datahandle;
+    # }
+    fmt = ">ccccihhiiii"
+    fields = struct.unpack(fmt, h.read(28))
+    direntry = {'name': fields[0] + fields[1] + fields[2] + fields[3],
+                'number': fields[4],
+                'elementtype': fields[5],
+                'elementsize': fields[6],
+                'numelements': fields[7],
+                'datasize': fields[8],
+                'dataoffset': fields[9]}
+    # If the data in this entry is no larger than 4 bytes, it is
+    # stored directly in the dataoffset field instead of putting it
+    # elsewhere and storing a pointer to it.
+    if direntry['datasize'] <= 4:
+        # We rencode the dataoffset field to be decoded by the
+        # appropriate function.
+        h2 = packed_handle('>i', direntry['dataoffset'])
+        direntry['data'] = read_sequence(direntry, h2)
+    assert direntry['datasize'] >= direntry['numelements'] * direntry['elementsize']
+    return direntry
+
+def test_parse_direntry():
+    tdir = packed_handle('>ccccihhiiii',
+                         't', 'd', 'i', 'r',
+                         1, 1023, 28, 10, 280, 10233, 30)
+    assert parse_direntry(tdir) == {'name': 'tdir', 'number': 1,
+                                    'elementtype': 1023, 'elementsize': 28,
+                                    'numelements': 10, 'datasize': 280,
+                                    'dataoffset': 10233}
+
+
+def read_sequence(direntry, handle):
+    """Read the data encoded by *direntry* from *handle.
+
+    The *direntry* should be a dictionary of the kind returned by
+    ``parse_direntry``.  It properly loops through the provided
+    handle, reading elements of type specified in direntry, then
+    returns those elements as a list.
+    """
+    decoder = _element_decoders[direntry['elementtype']]
+    values = []
+    bytes_read = 0
+    while bytes_read < direntry['datasize']:
+        (val, n_read) = decoder(handle, direntry['elementsize'])
+        values.append(val)
+        bytes_read += n_read
+    return values
+
+
+@contextlib.contextmanager
+def acquire(lock):
+    """Wrap acquiring and releasing a lock in a with block.
+
+    This should be used as::
+
+        with acquire(mylock):
+            ...do things needing the locked resources
+    """
+    lock.acquire()
+    try:
+        yield
+    finally:
+        lock.release()
+
+
+class SequenceWithConfidence(object):
+    def __init__(self, sequence, confidences):
+        assert len(sequence) == len(confidences)
+        self.sequence = sequence
+        self.conf = confidences
+    def __len__(self):
+        return len(self.sequence)
+    def __getitem__(self, i):
+        return SequenceWithConfidence(self.sequence[i], [self.conf[i]])
+    def __getslice__(self, i, j):
+        return SequenceWithConfidence(self.sequence[i:j], self.conf[i:j])
+    def __str__(self):
+        return ' '.join(['%s/%s' % (b,c) for b,c in zip(self.sequence, self.conf)])
+    def __repr__(self):
+        return ' '.join(['%s/%s' % (b,c) for b,c in zip(self.sequence, self.conf)])
+    def __add__(self, other):
+        if isinstance(other, SequenceWithConfidence):
+            return SequenceWithConfidence(self.sequence + other.sequence,
+                                          self.conf + other.conf)
+        elif isinstance(other, basestring):
+            return self.sequence + other
+        else:
+            raise ValueError("Invalid thing to add to SequenceWithConfidence")
+    def reverse(self):
+        return SequenceWithConfidence(self.sequence[::-1], self.conf[::-1])
+    def complement(self):
+        new_seq = self.sequence. \
+            replace('A','t'). \
+            replace('T','a'). \
+            replace('C','g'). \
+            replace('G','c').upper()
+        return SequenceWithConfidence(new_seq, self.conf[::-1])
+    def revcomp(self):
+        return self.reverse().complement()
+    def high_quality_segment(self, threshold=40):
+        high_quality_positions = [i for i,x in enumerate(self.conf)
+                                  if x > threshold]
+        if len(high_quality_positions) < 10:
+            return SequenceWithConfidence('',[])
+        else:
+            high_quality_positions.reverse()
+            while True:
+                if len(high_quality_positions) < 10:
+                    return SequenceWithConfidence('',[])
+                start = high_quality_positions.pop()
+                if all([x > threshold for x in self.conf[start:(start+5)]]):
+                    break
+            high_quality_positions.reverse()
+            while True:
+                if len(high_quality_positions) < 10:
+                    return SequenceWithConfidence('',[])
+                end = high_quality_positions.pop()
+                if all([x > threshold for x in self.conf[(end-5):end]]):
+                    break
+            return self[start:end]
+
+def reapply_confidences(seq, oldseq):
+    old_conf = list(reversed(oldseq.conf)) # pop goes from the end in Python
+    new_conf = []
+    for c in seq:
+        if c == '-':
+            new_conf.append(None)
+        else:
+            new_conf.append(old_conf.pop())
+    return SequenceWithConfidence(seq, new_conf)
+
+
+
+class Ab1File(object):
+    """Open an AB1 file for reading.
+
+    The ``Ab1File`` object imitates a read only dictionary by providing ``keys()`` and lookup with ``[]``.  The result is always a list of lists.  The inner lists contain the actual values, the outer lists are each instance of a key in the file, in order.
+
+    This class is thread safe *if* no other code uses the handle it
+    uses.  Internally, all operations properly lock the handle before
+    seeking to fetch their data.
+
+    The class does not cache data read from disk.
+    """
+    def __init__(self, handle):
+        self.handle = handle
+        self.lock = threading.Lock()
+        ab1_header = parse_header(self.handle)
+        self.version = ab1_header['version']
+        # Read enough information on each entry from disk to be able
+        # to fetch it on demand.  The name of the entries is not
+        # unique, so we store them in an association list.
+        self.entries = collections.defaultdict(list)
+        self.handle.seek(ab1_header['dataoffset'])
+        for i in range(ab1_header['numelements']):
+            direntry = parse_direntry(self.handle)
+            key = direntry['name']
+            self.entries[key].append(direntry)
+        self.base_order = dict([(chr(x).upper(),i) 
+                                for i,x in enumerate(self['FWO_'][0])])
+
+    def keys(self):
+        return self.entries.keys()
+
+    def __len__(self):
+        """Return the number of entries in the file.
+
+        Note that this is *not* the same as the number of distinct
+        keys.  It is the number of instances of each key.
+        """
+        return sum(len(self.entries[k]) for k in self.keys())
+
+    def __getitem__(self, key):
+        """Fetch the values listed under name *key* as a list.
+
+        The values are fetched from disk each time they are requested.
+        No caching is attempted.
+        """
+        if not(key in self.entries):
+            raise KeyError("No such key %s in AB1 file" % key)
+        data = []
+        for direntry in self.entries[key]:
+            if 'data' in direntry: # The data is <= 4 bytes long, and stored locally
+                data.append(direntry['data'])
+            else: # The data must be fetched from disk
+                with acquire(self.lock):
+                    self.handle.seek(direntry['dataoffset'])
+                    data.append(read_sequence(direntry, self.handle))
+        return data
+
+    def __iter__(self):
+        return self.entries.iteritems()
+
+    def iteritems(self):
+        for k in self.keys():
+            for v in self[k]:
+                yield (k,v)
+    def itervalues(self):
+        for k in self.keys():
+            for v in self[k]:
+                yield v
+    def iterkeys(self):
+        return self.entries.iterkeys()
+
+
+    def bases(self):
+        return ''.join(chr(x) for x in self['PBAS'][0])
+    def base_centers(self):
+        return self['PLOC'][0]
+    def base_confidences(self):
+        return self['PCON'][0]
+    def bases_with_confidence(self):
+        return SequenceWithConfidence(self.bases(), self.base_confidences())
+    def trace(self, base):
+        assert base in 'ACTG'
+        return self['DATA'][self.base_order[base] + 8]
+    def _raw_trace(self, base):
+        assert base in 'ACTG'
+        return self['DATA'][self.base_order[base]]
+    def signal_strength(self, base):
+        assert base in 'ACTG'
+        return self['S/N%'][0][self.base_order[base]]
+    def run_date(self):
+        return self['RUND'][0][0]
+    def run_time(self):
+        return self['RUNT'][0][0]
+
+def test_ab1file():
+    with open('scrapings/tmpClCBJg-1.ab1', 'rb') as h:
+        a = Ab1File(h)
+        assert len(a) == 122 
+        assert a['Scan'] == [[13960]]
+        assert a['CTTL'] == [['Comment:']]
+        assert isinstance(a.bases(), str)
+        assert isinstance(a.base_centers(), list)
+        assert isinstance(a.base_confidences(), list)
+        assert all([x > 0 and x < 100 for x in a.base_confidences()])
+        assert isinstance(a.trace('A'), list)
+        assert isinstance(a._raw_trace('A'), list)
+
+
---
+++ seqviewer/contig.py
@@ -0,0 +1,163 @@
+import Bio.pairwise2
+import ab1
+import sys
+import re
+
+def as_key(s):
+    return tuple(sorted(set(s)))
+
+iupac = {('A','C'): 'M',
+         ('A','G'): 'R',
+         ('A','T'): 'W',
+         ('C','G'): 'S',
+         ('C','T'): 'Y',
+         ('G','T'): 'K',
+         ('A',): 'A',
+         ('C',): 'C',
+         ('T',): 'T',
+         ('G',): 'G',
+         ('A','C','G'): 'V',
+         ('A','C','T'): 'H',
+         ('A','G','T'): 'D',
+         ('C','G','T'): 'B',
+         ('A','C','G','T'): 'N'}
+
+iupac_table = {}
+for k1,v1 in iupac.iteritems():
+    for k2,v2 in iupac.iteritems():
+        new_key = as_key([v1,v2])
+        new_base = iupac[as_key(set(k1).union(k2))]
+        iupac_table[new_key] = new_base
+for i in iupac.itervalues():
+    iupac_table[(i,)] = i
+    iupac_table[('-',i)] = i
+    iupac_table[('.',i)] = i
+iupac_table[tuple()] = 'N'
+
+def mask_poor_bases(s, threshold=40):
+    new_seq = ''
+    for i in range(len(s)):
+        if s.conf[i] > threshold:
+            new_seq += s.sequence[i]
+        else:
+            new_seq += 'N'
+    assert len(s) == len(new_seq)
+    return new_seq
+    
+
+def merge(s1, s2, min_segment_len=20, quality_threshold=40):
+    # s1 and s2 are SequenceWithConfidence objects containing the two reads.  The preconditions are:
+    #   * s1 is a noisy subsequence of some template sequence x, and s2 is a noisy subsequence of revcomp(x).
+    #   * s1 and s2 both consist of two low quality segments flanking a (possibly empty) high quality segment.
+    #   * s1 and s2 are words over the alphabet AGCTN.
+    #   * s1 and s2 may contain incorrect bases and indels.
+
+    # First we extract the high quality segment in the middle.
+    # Extremely short high quality segments are not useful to us, so
+    # we put a threshold on the lowest length of interest to us.  If
+    # we cannot get a long enough segment out of either sequence, our
+    # sequence estimation fails.  If only one sequence has a long
+    # enough high quality segment, we use its high quality segment and
+    # ignore the other sequence entirely.
+    h1 = s1.high_quality_segment(threshold=quality_threshold)
+    h2 = s2.high_quality_segment(threshold=quality_threshold).revcomp()
+    if len(h1) < min_segment_len and len(h2) < min_segment_len:
+        return None
+    elif len(h1) < min_segment_len:
+        # But we don't want to accept low quality bases.  We set them
+        # back to N if the quality is below threshold.  If there are
+        # more than 15% N's in the sequence, we fail.
+        masked = mask_poor_bases(h2, quality_threshold)
+        if len([x for x in masked if x == 'N'])/float(len(masked)) > 0.15:
+            return None
+        else:
+            return (masked,
+                    ''.join(['-' for i in range(len(h2))]), 
+                    h2.sequence)
+    elif len(h2) < min_segment_len:
+        masked = mask_poor_bases(h1, quality_threshold)
+        if len([x for x in masked if x == 'N'])/float(len(masked)) > 0.15:
+            return None
+        else:
+            return (masked,
+                    h1.sequence,
+                    ''.join(['-' for i in range(len(h1))]))
+    else:
+        pass
+
+    # At this point h1 and h2 are a pair of high quality segments of
+    # adequate length.  We align them locally, imposing a gap penalty
+    # to keep each sequence clumped together.  This produces a1 and
+    # a2, which are aligned, have the same length, and are words over
+    # the alphabet ACGTN-.
+    alignment = Bio.pairwise2.align.localxs(h1.sequence, h2.sequence, -5, -10)[0]
+    # Bio.pairwise2 doesn't understand SequenceWithConfidence objects,
+    # so we have to reapply the confidences in order to proceed.
+    assert(isinstance(alignment[0], str))
+    assert(isinstance(alignment[1], str))
+    a1 = ab1.reapply_confidences(alignment[0], h1)
+    a2 = ab1.reapply_confidences(alignment[1], h2)
+
+    # Finally we choose a base at each position by a two pass voting
+    # algorithm.  In the first round, each sequence votes on whether
+    # this base should be skipped.  '-' is a vote to skip, anything
+    # else is a vote to insert.  A vote with low confidence is ignored
+    # if there is a vote to skip.  Upon skipping, an 'N' is
+    # accumulated, and upon the next vote not to skip, the number of
+    # accumulated N's is counted.  If there are at least 2 N's, then
+    # the N's are inserted in the sequence.  Otherwise the accumulated
+    # N's are abandoned.
+
+    # If there is a second round, that is, if the base is not skipped,
+    # then there is a vote on what base it is.  A low confidence base
+    # leads to an ignored vote.  High confidence bases are combined
+    # into their IUPAC codes, ignoring any vote of '-'.
+
+    # This all applies during the overlap of the sequences.  In end
+    # regions where there is only one sequence, we need to behave
+    # differently.  In this case, we replace the -'s at the beginning
+    # and end with .'s, representing uncalled bases.  A dot does not
+    # vote for skipping, but also does not vote for a base.
+    offset = len(re.split(r'[^-]', a1.sequence, maxsplit=1)[0])
+    if offset > 0:
+        a1.sequence = ''.join(['.' for i in range(offset)]) + a1.sequence[offset:]
+    offset = len(re.split(r'[^-]', a2.sequence, maxsplit=1)[0])
+    if offset > 0:
+        a2.sequence = ''.join(['.' for i in range(offset)]) + a2.sequence[offset:]
+    offset = len(re.split(r'[^-]', a1.sequence[::-1], maxsplit=1)[0])
+    if offset > 0:
+        a1.sequence = a1.sequence[:(-offset)] + ''.join(['.' for i in range(offset)])
+    offset = len(re.split(r'[^-]', a2.sequence[::-1], maxsplit=1)[0])
+    if offset > 0:
+        a2.sequence = a2.sequence[:(-offset)] + ''.join(['.' for i in range(offset)])
+
+    y = ''
+    accum = ''
+    for i in range(len(a1)): # len(a1) == len(a2)
+        base1 = a1.sequence[i]
+        base2 = a2.sequence[i]
+        conf1 = a1.conf[i]
+        conf2 = a2.conf[i]
+        
+        # Voting round 1: skipping
+        if (base1 == '-' or base2 == '-') and \
+                max(conf1,conf2) <= quality_threshold:
+            accum += 'N'
+            continue
+        else: # if not skipping, try to insert N's
+            if len(accum) > 1:
+                y += accum
+            accum = ''
+
+        # Voting round 2: base call
+        base_key = set()
+        if conf1 > quality_threshold:
+            base_key.update([base1])
+        if conf2 > quality_threshold:
+            base_key.update([base2])
+        if base1 == base2 and conf1 + conf2 > quality_threshold:
+            base_key.update([base1])
+        call = iupac_table[as_key(base_key)]
+        y += call
+    return (y, a1.sequence, a2.sequence)
+
